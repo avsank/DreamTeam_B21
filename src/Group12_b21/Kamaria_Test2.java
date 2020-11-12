17976e092bab8a1f0a8276848261a3755384727e
Kamariaellerbe <ke.stem2021@gmail.com>
2020-11-12 15:54:43 -0500

    Group12 Test Repo

---
+++ src/Group12_b21/Kamaria_Test2.java
@@ -0,0 +1,9 @@
+package Group12_b21;
+
+public class Kamaria_Test2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("This one better work too.");
+    }
+}
---
+++ src/Group12_b21/Kamaria_TestFile.java
@@ -0,0 +1,12 @@
+package Group12_b21;
+
+public class Kamaria_TestFile {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hope this works, and makes ya smile.");
+        System.out.println("I hope it makes you feel Confident!");
+
+    }
+
+}
